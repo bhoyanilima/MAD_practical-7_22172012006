362710d9875cb5ff7d825bfb8fb6c3edc2317aa6
Student <student@gnu.ac.in>
2023-09-20 08:57:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_7_22172012006/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mad_practical_7_22172012006
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.MediaController
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mediaController = MediaController(this)
+        val uri: Uri = Uri.parse("android.resource://" + packageName +"/" + R.raw.thestoryoflight)
+    }
+}
